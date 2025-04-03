87b6b7986df160c81a7c29feb9fed9754326528e
pthomas-44 <thomas.tech44@gmail.com>
2025-04-03 16:21:11 +0200

    + Copie le contenu de l'extension et lance un nested gnome-shell

--- debug.sh
+++ debug.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+cp extension/* ~/.local/share/gnome-shell/extensions/nextbus@42toolbar/.
 SHELL_DEBUG=all
 #Used to spawn a nested gnome-shell for debugging purposes
 dbus-run-session -- gnome-shell --nested --wayland
