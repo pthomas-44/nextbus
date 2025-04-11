6d4ff8e7089ead934e3a294d23b41a5a0888c434
SimonCROS <scros@student.42lyon.fr>
2025-04-11 17:14:27 +0200

    Fix missing parantheses around bus time

--- extension/extension.js
+++ extension/extension.js
@@ -124,7 +124,7 @@ function busToString(bus, isPreview) {
     if (!isPreview) {
         const hours = Math.floor(bus.normalizedTime / 60);
         const minutes = bus.normalizedTime % 60;
-        formattedParts.push(`${String(hours).padStart(2, "0")}:${String(minutes).padStart(2, "0")}`);
+        formattedParts.push(`(${String(hours).padStart(2, "0")}:${String(minutes).padStart(2, "0")})`);
     }
 
     if (bus.isLast) {
