25d3bc7a612565690aad5a8052193717c496d6b2
SimonCROS <scros@student.42lyon.fr>
2025-04-16 17:24:16 +0200

    Start implementing a sqlite databse for querying data

---
+++ extension/get_times.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+DB_NAME=${1:-"tcl_gtfs.sqlite"}
+STOP_NAME=${2:-"Campus Région numérique"}
+HEADSIGN=${3:-"Gorge de Loup"}
+ROUTE_NAME=${4:-"86"}
+DATE=${5:-"2025-04-15"}
+
+sed \
+  -e "s/{{STOP_NAME}}/${STOP_NAME}/g" \
+  -e "s/{{HEADSIGN}}/${HEADSIGN}/g" \
+  -e "s/{{ROUTE_NAME}}/${ROUTE_NAME}/g" \
+  -e "s/{{DATE}}/${DATE}/g" \
+  sql/today_times_query.sql | sqlite3 "$DB_NAME"
